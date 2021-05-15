4b5055b0a5621cb771bf0e420ba703a90e3215c8
Prabhjot Rai <psr@captain.local>
2021-05-15 09:42:03 +0530

    feat: create fn for reading as consumer indefinitely

--- redisutil.go
+++ redisutil.go
@@ -3,6 +3,8 @@ package goc
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"github.com/go-redis/redis/v8"
 )
 
@@ -29,4 +31,48 @@ func GetClient(connection RedisConnection) *redis.Client {
 func TestConnection(client *redis.Client) (string, error) {
   pong, err := client.Ping(ctx).Result()
   return pong, err
+}
+
+func XReadGroup(xReadGroupArgs *redis.XReadGroupArgs, redis *redis.Client) *redis.XStreamSliceCmd {
+  return redis.XReadGroup(ctx, xReadGroupArgs)
+}
+
+type ProcessMessage func(string)
+
+// Reads as a consumer in consumer group from a
+// given stream indefinitely
+func XReadGroupBlocking(xReadGroupArgs *redis.XReadGroupArgs, redis *redis.Client, processMessage ProcessMessage) {
+  if (XReadGroupArgs.Block != time.ParseDuration("0s")) {
+    panic("XReadGroupBlocking should have indefinite block time (0s).")
+  }
+  for true {
+    results, err := XReadGroup(XReadGroupArgs, client).Result()
+    if (err != redis.Nil || err != nil) {
+      panic(fmt.Sprintf("GOC::Redis Error %v", err))
+    }
+    if (len(results) > 0) {
+      for _, result := range results {
+        stream := results.Stream
+        for _, message := range result.Messages {
+          processMessage(message)
+          updateIndex = -1
+          currentStreams := xReadGroupArgs.Streams()
+          for index, str := range currentStreams {
+            if (str == stream) {
+              updateIndex = index + 1
+              break
+            }
+          }
+          currentStreams[updateIndex] = stream
+          xReadGroupArgs := &redis.XReadGroupArgs{
+            Group: xReadGroupArgs.Group,
+            Consumer: xReadGroupArgs.Consumer,
+            Streams: currentStreams,
+            Count: xReadGroupArgs.Count,
+            Block: xReadGroupArgs.Block,
+            NoAck: xReadGroupArgs.NoAck,
+          }
+        }
+      }
+    }
 }
